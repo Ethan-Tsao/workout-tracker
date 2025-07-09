fcb75ca79ed40f01773940470db64c937752018c
Ethan-Tsao <tsao.ethan33@gmail.com>
2025-07-09 11:53:12 -0500

    fixed typing issue in template api

--- app/api/templates/[id]/route.ts
+++ app/api/templates/[id]/route.ts
@@ -1,8 +1,11 @@
-// /app/api/templates/[id]/route.ts
-import { NextResponse } from "next/server";
+// app/api/templates/[id]/route.ts
+import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
-export async function DELETE(_: Request, { params }: { params: { id: string } }) {
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
   const id = Number(params.id);
   await prisma.template.delete({ where: { id } });
   return NextResponse.json({ ok: true });
